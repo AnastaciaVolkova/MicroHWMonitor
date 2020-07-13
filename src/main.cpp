3577206f97fcf96cfed13eafd9696bcc2991db7d
AnastaciaVolkova <polarbolide@gmail.com>
2020-07-13 20:25:30 +0200

    feat: Add possibility to catch user input

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <chrono>
 #include <fstream>
-
+#include <cmath>
 #include "read_write.hpp"
 #include "tinyxml2.h"
 #include <stdexcept>
@@ -10,6 +10,7 @@
 #include <vector>
 #include "transform.hpp"
 #include <memory>
+#include <future>
 
 using std::invalid_argument;
 using std::ofstream;
@@ -17,6 +18,29 @@ using std::string;
 using std::to_string;
 using std::unique_ptr;
 using std::vector;
+using std::chrono::milliseconds;
+using std::chrono::system_clock;
+
+// Function to show number of processed samples
+void ShowSamples(int sample_number, bool to_r)
+{
+  auto cyc = [](char c, int m) {
+    for (int i = 0; i < m; i++)
+      std::cout << c;
+    std::cout.flush();
+  };
+  string xstr = to_string(sample_number);
+  string info = "Number of read samples: ";
+  cyc(' ', info.size() + xstr.size() + 1);
+  cyc('\r', info.size() + xstr.size() + 1);
+
+  std::cout << info << sample_number;
+
+  if (to_r)
+    cyc('\r', info.size() + xstr.size() + 1);
+  else
+    std::cout << std::endl;
+};
 
 int main(int argc, char *argv[])
 {
@@ -50,40 +74,48 @@ int main(int argc, char *argv[])
 
   vector<float> x;
 
+  int sample_number = 0;
+
+  // Run thread to catch user input
+  auto key = std::async(std::launch::async, []() {string key; std::cin >> key; return key; });
+
+  // Time, when iteration starts
   auto starts = std::chrono::system_clock::now();
 
-  int sample_number = 0;
+  // Main cycle
   while (true)
   {
-    if (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - starts).count() >= tp)
+    sample_number++;
+    float freq = ds->GetValue();
+    x.push_back(freq);
+    if (x.size() == batch_num)
     {
+      if (transform == "fft")
+      {
+        vector<float> y;
+        Transformer::fft(x, y);
+        writer->WriteData(y);
+      }
+      else
+      {
+        writer->WriteData(x);
+      }
+      x.clear();
+    }
+    if ((sample_number % 10) == 0)
+    {
+      ShowSamples(sample_number, true);
+      // Wait rest of time for user keystroke
+      system_clock::time_point tw = starts + milliseconds(static_cast<int>(round(tp)));
+      std::future_status status = key.wait_until(tw);
+      if (status == std::future_status::ready)
+      {
+        ShowSamples(sample_number, false);
+        break;
+      }
+      while (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - starts).count() <= tp)
+        ;
       starts = std::chrono::system_clock::now();
-      sample_number++;
-      float freq = ds->GetValue();
-      x.push_back(freq);
-      if (x.size() == batch_num)
-      {
-        if (transform == "fft")
-        {
-          vector<float> y;
-          Transformer::fft(x, y);
-          writer->WriteData(y);
-        }
-        else
-        {
-          writer->WriteData(x);
-        }
-        x.clear();
-      }
-      if ((sample_number % 10) == 0)
-      {
-        string xstr = to_string(sample_number);
-        string info = "Number of read samples: ";
-        std::cout << info << sample_number;
-        for (int i = 0; i < info.size() + xstr.size(); i++)
-          std::cout << "\r";
-        std::cout.flush();
-      }
     }
   }
   return 0;
