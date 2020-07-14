cf3088f3c81f857c20a754b3f0302fc92b6be8cc
AnastaciaVolkova <polarbolide@gmail.com>
2020-07-14 19:45:04 +0200

    feat: Change transformations so that it handles input 2d data

--- src/transform.cpp
+++ src/transform.cpp
@@ -6,11 +6,12 @@ using std::vector;
 void FFTTransformer::operator()(vector<float> &in_data, vector<vector<float>> &out_data)
 {
   fftw_complex *in, *out;
+  int n = in_data.size();
   in = (fftw_complex *)fftw_malloc(sizeof(fftw_complex) * in_data.size());
   out = (fftw_complex *)fftw_malloc(sizeof(fftw_complex) * in_data.size());
   out_data.resize(2 * in_data.size());
   fftw_plan p = fftw_plan_dft_1d(64, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
-  for (int i = 0; i < 64; i++)
+  for (int i = 0; i < n; i++)
   {
     in[i][0] = in_data[i];
     in[i][1] = 0;
@@ -19,7 +20,7 @@ void FFTTransformer::operator()(vector<float> &in_data, vector<vector<float>> &o
   out_data.resize(2);
   out_data[0].resize(in_data.size());
   out_data[1].resize(in_data.size());
-  for (int i = 0; i < in_data.size(); i++)
+  for (int i = 0; i < n; i++)
   {
     out_data[0][i] = out[i][0];
     out_data[1][i] = out[i][1];
@@ -33,18 +34,19 @@ void FFTTransformer::operator()(vector<float> &in_data, vector<vector<float>> &o
 void IFFTTransformer::operator()(vector<float> &in_data, vector<vector<float>> &out_data)
 {
   fftw_complex *in, *out;
+  int n = in_data.size() / 2;
   in = (fftw_complex *)fftw_malloc(sizeof(fftw_complex) * in_data.size());
   out = (fftw_complex *)fftw_malloc(sizeof(fftw_complex) * in_data.size());
   out_data.resize(1);
-  out_data[0].resize(in_data.size());
-  fftw_plan p = fftw_plan_dft_1d(64, in, out, FFTW_BACKWARD, FFTW_ESTIMATE);
-  for (int i = 0; i < 64; i++)
+  out_data[0].resize(n);
+  fftw_plan p = fftw_plan_dft_1d(n, in, out, FFTW_BACKWARD, FFTW_ESTIMATE);
+  for (int i = 0; i < n; i++)
   {
     in[i][0] = in_data[2 * i];
     in[i][1] = in_data[2 * i + 1];
   }
   fftw_execute(p);
-  for (int i = 0; i < out_data.size(); i++)
+  for (int i = 0; i < n; i++)
     out_data[0][i] = out[i][0];
 
   fftw_destroy_plan(p);
